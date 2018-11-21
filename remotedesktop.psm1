19d538cd2354f78756fdaaf526411432595c2093
Buenting <600990@bah.com>
2018-11-21 14:55:28 -0500

    initial update

---
+++ remotedesktop.psm1
@@ -0,0 +1,31 @@
+﻿Function Out-RDMenuFile {
+
+    <#
+        .Synopsis
+            Creates a Remote Desktop Menu HTML File.
+
+        .Description
+            This is the HTML file with a list of servers and hyper links to open a remote desktop connection
+
+        .Link
+            https://stackoverflow.com/questions/27652761/convert-a-list-of-urls-into-clickable-html-links-using-convertto-html
+
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter ( ValueFromPipeLine = $True, Mandatory = $True )]
+        [PSObject[]]$Server,
+
+        [String]$Path
+    )
+
+    process {
+        Foreach ( $S in $Server ) {
+            Write-Verbose "Converting $($S.Name)"
+
+
+        }
+    }
+
+}
